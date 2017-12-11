cb14cba129cea264de82a660e695f156da007663
HarasawaNaoya <rururulily@gmail.com>
2017-12-12 00:09:16 +0900

    VueJS+OnsenUI環境づくりまで完了

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">{{ title }}</div>
+      <div class="right">
+        <v-ons-toolbar-button>
+          <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
+        </v-ons-toolbar-button>
+      </div>
+    </v-ons-toolbar>
+
+    <p style="text-align: center">
+      <v-ons-button @click="$ons.notification.alert('Hello World!')">
+        Click me!
+      </v-ons-button>
+      <!-- 画像表示テスト
+      <img src="./assets/images/bottom_01.png" />
+      -->
+    </p>
+  </v-ons-page>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+// This already imports 'onsenui'
+import VueOnsen from 'vue-onsenui'; 
+Vue.use(VueOnsen);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
